d1ab8847abc2b99cdc013f9fd501546969d1a015
EugenM03 <geanyzz91@gmail.com>
2023-04-06 10:05:27 +0300

    Fix comments

--- struct.h
+++ struct.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <stdbool.h>
+#include <inttypes.h>
 
 typedef struct piece {
   char color;  // piece's color
   char type;   // piece's type
 
   // Pointer to a function that checks if the piece can move to the given
-  // position and returns TRUE if it can as well as moving the piece to that
-  // qwewqe position. Otherwise, returns FALSE
+  // position and returns TRUE if it can. Otherwise, returns FALSE
   bool (*move)(piece ***, uint8_t[2], uint8_t[2]);
 } piece;
